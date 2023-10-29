7297aca64e7e6ebcfbe038be685aa71ab55ad837
Alkhanet <92195275+alkhanet26@users.noreply.github.com>
2023-10-29 23:52:30 +0700

    Add files via upload

---
+++ geto.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This Script by Alkhanet (http://github.com/alkhanet26)
+
+
+{
+pkg=$(opkg list | grep -o screen)
+echo -e "Checking Supporting Packages"
+
+if [ $pkg = "screen" ]; then
+	echo "Downloading Binaries"
+	wget -q --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/modemnet/main/lednet" -O /sbin/lednet && chmod +x /sbin/lednet
+	wget -q --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/modemnet/main/ledon" -O /sbin/ledon && chmod +x /sbin/ledon
+	wget -q --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/modemnet/main/modemnet" -O /sbin/modemnet && chmod +x /sbin/modemnet
+	sleep 2
+	echo "Successfully Downloaded"
+	sleep 5
+	clear
+	bash --login
+else
+	echo "Downloading Packages"
+	opkg update && opkg install screen
+	sleep 2
+	clear
+	wget -q --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/modemnet/main/lednet" -O /sbin/lednet && chmod +x /sbin/lednet
+	wget -q --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/modemnet/main/ledon" -O /sbin/ledon && chmod +x /sbin/ledon
+	wget -q --no-check-certificate "https://raw.githubusercontent.com/alkhanet26/modemnet/main/modemnet" -O /sbin/modemnet && chmod +x /sbin/modemnet
+	sleep 2
+	echo "Successfully Downloaded"
+	sleep 5
+	clear
+	bash --login
+fi
+}
